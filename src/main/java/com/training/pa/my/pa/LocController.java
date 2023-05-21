4dd453330c1e8d032414c4164569b86a2bdfb351
mj325a <mj325a@att.com>
2023-05-21 17:13:34 -0500

    add cont

---
+++ src/main/java/com/training/pa/my/pa/LocController.java
@@ -0,0 +1,12 @@
+package com.training.pa.my.pa;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pa/loc")
+public class LocController {
+
+
+}
+
